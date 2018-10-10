e2130342aaa194b093868f9a6b06bf62be92aa80
khramovw <khramovw@gmail.com>
2018-10-10 16:15:54 +0300

    portfolio

--- js/plugin.js
+++ js/plugin.js
@@ -10,19 +10,82 @@ function htmlcolToObj(arr) {
     return [].slice.call(arr);
 }
 
-var MainNav = function () {
-    function MainNav() {
-        _classCallCheck(this, MainNav);
+// class MainNav {
+//     constructor () {
+//         this.menubtn = document.querySelector('.menu-js');
+//         this.menulistitem = document.querySelector('.menu-items-js');
+//         this.navitems = htmlcolToObj(document.querySelectorAll('.menu-items-js ul li'));
+//         // this.content = document.querySelector('.open-menu-js');
+//     }
+//     init () {
+//         this.setactive();
+//         this.eventIs ();
+//     }
+//     eventIs () {
+//         this.menubtn.addEventListener('click', e => {
+//             e.preventDefault();
+//             this.toggleOpenMenu();
+//         });
+//     }
+//     toggleOpenMenu () {
+//         // TweenMax.staggerFrom('.menu-items-js ul li a', .1, { opacity: 0, scale: .5, y: -80, ease:Back.easeOut.config(3) }, .2);
+//         // PubSub.publish('openmenu', { items: this.navitems });
+//         this.content = document.querySelector('.open-menu-js');
+//         this.menubtn.classList.toggle('open-menu');
+//         this.menulistitem.classList.toggle('open-menu');
+//         this.content.classList.toggle('open-menu');
+//     }
+//     setactive () {
+//         // Gsap
+//         let tl = new TimelineMax();
+//         this.navitems.some( el => {
+//             el.addEventListener('click', link => {
+//                 this.resetactive();
+//                 link.target.parentElement.classList.add('active-item');
+//                 this.toggleOpenMenu();
+//             })
+//         });
+//     }
+//     resetactive () {
+//         this.navitems.filter( el => el.classList.remove('active-item'));
+//     }
+//
+// }
+
+var PageNavigate = function () {
+    function PageNavigate() {
+        _classCallCheck(this, PageNavigate);
+
+        this.self = this;
 
         this.menubtn = document.querySelector('.menu-js');
         this.menulistitem = document.querySelector('.menu-items-js');
         this.navitems = htmlcolToObj(document.querySelectorAll('.menu-items-js ul li'));
-        // this.content = document.querySelector('.open-menu-js');
+
+        this.pages = [];
+        this.canGo = true;
+        this.currentPageNum = 1;
+        this.currentPage;
+        this.nextPage;
+        this.nextPageNum;
+        this.navtopage;
+
+        this.scrollEvent();
+        this.clickEvent();
+        this.mainnav();
+
+        this.allpages();
+        this.maxpage = this.pages.length;
+
+        this.pagenavigating();
     }
 
-    _createClass(MainNav, [{
-        key: 'init',
-        value: function init() {
+    // Main navigation start
+
+
+    _createClass(PageNavigate, [{
+        key: 'mainnav',
+        value: function mainnav() {
             this.setactive();
             this.eventIs();
         }
@@ -39,32 +102,47 @@ var MainNav = function () {
     }, {
         key: 'toggleOpenMenu',
         value: function toggleOpenMenu() {
-            // this.menubtn.addEventListener('click', e => {
-            //     e.preventDefault();
-            //     // this.content = document.querySelector('.open-menu-js');
-            //     // this.menubtn.classList.toggle('open-menu');
-            //     // this.menulistitem.classList.toggle('open-menu');
-            //     // this.content.classList.toggle('open-menu');
-            //     // TweenMax.staggerFrom('.menu-items-js ul li a', .1, { opacity: 0, scale: .5, y: -80, ease:Back.easeOut.config(3) }, .2);
-            //     // PubSub.publish('openmenu', { items: this.navitems });
-            // });
+            var tl = new TimelineMax();
+            // TweenMax.staggerFrom('.menu-items-js ul li a', .1, { opacity: 0, scale: .5, y: -80, ease:Back.easeOut.config(3) }, .2);
+            // PubSub.publish('openmenu', { items: this.navitems });
             this.content = document.querySelector('.open-menu-js');
+
             this.menubtn.classList.toggle('open-menu');
             this.menulistitem.classList.toggle('open-menu');
             this.content.classList.toggle('open-menu');
+            tl.staggerFrom(document.querySelectorAll('.open-menu ul li'), 0.25, {
+                y: -20,
+                scale: 0,
+                autoAlpha: 0,
+                ease: Back.easeOut.config(2),
+                delay: .4 }, 0.1);
         }
     }, {
         key: 'setactive',
         value: function setactive() {
             var _this2 = this;
 
-            // Gsap
-            var tl = new TimelineMax();
             this.navitems.some(function (el) {
                 el.addEventListener('click', function (link) {
+
+                    // check active
+                    if (!link.target.dataset.target) return false;
+
+                    // Set active menu item
                     _this2.resetactive();
                     link.target.parentElement.classList.add('active-item');
+
                     _this2.toggleOpenMenu();
+
+                    // Go to preset page
+                    PubSub.publish('gotopage', {
+                        from: _this2.currentPageNum,
+                        to: parseFloat(link.target.dataset.target),
+                        pages: _this2.pages,
+                        currentPage: _this2.currentPage,
+                        nextPage: _this2.nextPage
+                    });
+                    _this2.currentPageNum = parseFloat(link.target.dataset.target);
                 });
             });
         }
@@ -75,36 +153,11 @@ var MainNav = function () {
                 return el.classList.remove('active-item');
             });
         }
-    }]);
+        // Main navigation end
 
-    return MainNav;
-}();
+        // get all pages
 
-var PageNavigate = function () {
-    function PageNavigate() {
-        _classCallCheck(this, PageNavigate);
-
-        this.pages = [];
-        this.canGo = true;
-        this.currentPageNum = 1;
-        this.currentPage;
-        this.nextPage;
-        this.nextpagenum;
-        this.navtopage;
-
-        this.scrollEvent();
-        this.clickEvent();
-
-        this.allpages();
-        this.maxpage = this.pages.length;
-
-        this.navigating();
-    }
-
-    // get all pages
-
-
-    _createClass(PageNavigate, [{
+    }, {
         key: 'allpages',
         value: function allpages() {
             var _this3 = this;
@@ -160,14 +213,14 @@ var PageNavigate = function () {
         // click
         value: function clickEvent() {}
     }, {
-        key: 'navigating',
-        value: function navigating() {
+        key: 'pagenavigating',
+        value: function pagenavigating() {
 
             // Gsap
             var tl = new TimelineMax();
 
             this.navtopage = PubSub.subscribe('gotopage', function (msg, data) {
-                // console.log(msg, data);
+                console.log(msg, data);
                 var currentPage = void 0,
                     nextPage = void 0;
 
@@ -176,14 +229,18 @@ var PageNavigate = function () {
                     page.dataset.page == data.to ? page.classList.add('is-active', 'open-menu-js') : page.classList.remove('is-active', 'open-menu-js');
                     page.dataset.page == data.to ? nextPage = page : true;
                     page.dataset.page == data.from ? currentPage = page : true;
-                    console.log('i: ', i, 'currentPage: ', currentPage, 'nextPage: ', nextPage);
+                    // console.log('i: ', i, 'currentPage: ', currentPage, 'nextPage: ', nextPage);
+                    // this.currentPage = currentPage;
+                    // this.nextPage = nextPage;
+                    // console.log(this.currentPage, this.nextPage);
+
                 });
 
                 if (data.to > data.from) {
-                    tl.fromTo(currentPage, 0.5, { x: '-50%', opacity: '1' }, { x: '-200%' }).to(currentPage, 0.1, { opacity: '0' }).fromTo(nextPage, 0.5, { x: '100%', opacity: '0' }, { x: '-50%', opacity: '1' });
+                    tl.fromTo(currentPage, 0.5, { x: '-50%', opacity: '1', autoAlpha: 1 }, { x: '-200%' }).to(currentPage, 0.1, { opacity: '0' }).fromTo(nextPage, 0.5, { x: '100%', opacity: '0', autoAlpha: 0 }, { x: '-50%', opacity: '1', autoAlpha: 1 });
                 }
                 if (data.to < data.from) {
-                    tl.fromTo(currentPage, 0.5, { x: '-50%', opacity: '1' }, { x: '200%' }).to(currentPage, 0.1, { opacity: '0' }).fromTo(nextPage, 0.5, { x: '-200%', opacity: '0' }, { x: '-50%', opacity: '1' });
+                    tl.fromTo(currentPage, 0.5, { x: '-50%', opacity: '1', autoAlpha: 1 }, { x: '200%' }).to(currentPage, 0.1, { opacity: '0' }).fromTo(nextPage, 0.5, { x: '-200%', opacity: '0', autoAlpha: 0 }, { x: '-50%', opacity: '1', autoAlpha: 1 });
                 }
 
                 // Changes Url
@@ -198,7 +255,7 @@ var PageNavigate = function () {
     return PageNavigate;
 }();
 
-var menu = new MainNav();
-menu.init();
+// let menu = new MainNav();
+// menu.init();
 
 var navtopage = new PageNavigate();
